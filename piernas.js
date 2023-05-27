e7486e6baaccf81cd03bcef191182c0fded3a9b3
ezequielleonzybert <ezequielleonzybert@gmail.com>
2023-05-27 18:32:34 -0300

    Update piernas.js

--- piernas.js
+++ piernas.js
@@ -1,65 +1,26 @@
-// Parámetros de las piernas
-const legHeight = 100;
-const legWidth = 20;
-const stepSize = 2;
+class Terrain {
 
-// Parámetros de la superficie irregular
-const terrainDetail = 0.2;
-let terrain;
+  constructor(res,hmin,hmax){
+    arraylist segments
+    let i =0;
+    while(i<w){
+      r1 = random(hmin, hmax)
+      r2 = random(hmin, hmax)
+      point p1 = point(i, r1)
+      i+=w*res
+      point p2 = point(i, r2)
+      segment = {"a":p1, "b":p2}
+      segments.pushback(segment)
+    }
+  }
 
-// Variables de control del movimiento
-let walking = false;
-let stepCounter = 0;
+  draw(){
+    for(each s in segments){
+      line(s.a, s.b)
+    }
+  }
 
-function setup() {
-  createCanvas(800, 400);
-
-  // Generar la superficie irregular
-  terrain = generateTerrain(width, height, terrainDetail);
-}
-
-function draw() {
-  background(220);
-
-  // Dibujar la superficie
-  drawTerrain();
-
-  // Dibujar las piernas
-  if (walking) {
-    walk();
-  } else {
-    drawLegs(width / 2, height - legHeight);
+  getSegment(a){
+    return segments[a]
   }
 }
-
-function mousePressed() {
-  walking = true;
-}
-
-function walk() {
-  // Realizar un paso hacia adelante
-  if (stepCounter < width) {
-    translate(stepSize, 0);
-    drawLegs(0, height - legHeight);
-    stepCounter += stepSize;
-  } else {
-    // Reiniciar la posición de las piernas y el contador de pasos
-    translate(-stepCounter, 0);
-    stepCounter = 0;
-    walking = false;
-  }
-}
-
-function drawLegs(x, y) {
-  // Dibujar las piernas en la posición dada
-  fill(0);
-  rect(x, y, legWidth, legHeight);
-  rect(x + legWidth, y, legWidth, legHeight);
-}
-
-function generateTerrain(width, height, detail) {
-  // Generar una matriz con valores de altura para simular una superficie irregular
-  const terrain = [];
-  let yoff = 0;
-
- 
